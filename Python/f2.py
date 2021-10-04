d1484bd5837e77caabb073eb46a829411de14125
Apurv Parashar <apoorv.parashar@gmail.com>
2021-10-04 22:06:55 +0530

    My First Commit and Push to Cloud Repo

---
+++ Python/f2.py
@@ -0,0 +1,4 @@
+Vim is a text editor for Unix that comes with Linux, BSD, and macOS. It is known to be fast and powerful, partly because it is a small program that can run in a terminal (although it has a graphical interface). It is mainly because it can be managed entirely without menus a = 20
+b = 10
+d = a+b
+print(d)
